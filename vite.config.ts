0fe2f9cf24bca2997a1681b194961a34fb970e7b
Sandile Mnqayi <wifizi.gokhinki@gmail.com>
2026-04-19 12:42:31 +0200

    Using .env for backend endpoint.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
         target: 'http://swiper-backend-production.up.railway.app:8080', // backend port
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/nothing/, '') // Strip '/api' prefix
+        rewrite: (path) => path.replace(/^\/api/, '') // Strip '/api' prefix
       }
     }
   }
